d99479b8fd86e888edf8ba3991d4d8dcf027afc3
Marvellro <marvellousjunioro@gmail.com>
2026-06-16 19:22:23 +0200

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+
+  if (code) {
+    return NextResponse.redirect(`${requestUrl.origin}/admin/waitlist`)
+  }
+
+  return NextResponse.redirect(`${requestUrl.origin}/login`)
+}
